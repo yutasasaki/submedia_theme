3ba326f52f28ef8c9de64112794c4700dca53dd6
EC2 Default User <ec2-user@ip-172-31-37-222.ap-northeast-1.compute.internal>
2024-01-04 09:59:19 +0000

    クリニックカセットの最適化

--- views/components/doctor.php
+++ views/components/doctor.php
@@ -1,13 +1,17 @@
+<?php if (!empty($clinic_data->director_name)) : ?>
 <!-- wp:heading {"level":4} -->
-<h4 class="wp-block-sgb-headings sgb-heading"><span class="sgb-heading__inner hh hh18" style="font-size:1.2em"><span class="sgb-heading__text"><?php echo esc_html($clinic_data->clinic_name); ?>専門医のご紹介</span></span></h4>
+<h4 class="wp-block-sgb-headings sgb-heading"><span class="sgb-heading__inner hh hh18" style="font-size:1.2em"><span class="sgb-heading__text"><?php echo esc_html($clinic_data->clinic_name); ?>の医師のご紹介</span></span></h4>
 <!-- /wp:heading -->
 
 <div class="wp-block-columns" style="gap: 20px;">
+
+  <?php if (!empty($clinic_data->director_photo_link)) : ?>
   <div class="wp-block-column" style="padding-right: 10px;">
     <figure class="wp-block-image size-large" style="width: 100%; height: 300px; overflow: hidden;">
-      <img src="<?php echo esc_url($clinic_data->director_photo_link); ?>" alt="<?php echo esc_attr($clinic_data->clinic_name); ?>の専門医<?php echo esc_attr($clinic_data->director_name); ?>先生" style="width: 100%; height: auto; max-height: 300px; object-fit: cover;" />
+      <img src="<?php echo esc_url($clinic_data->director_photo_link); ?>" alt="<?php echo esc_attr($clinic_data->clinic_name); ?>の医師の<?php echo esc_attr($clinic_data->director_name); ?>先生" style="width: 100%; height: auto; max-height: 300px; object-fit: cover;" />
     </figure>
   </div>
+  <?php endif; ?>
 
   <div class="wp-block-column" style="padding-left: 10px;">
     <!-- wp:heading {"level":5} -->
@@ -21,3 +25,4 @@
     <p><?php echo esc_html($clinic_data->director_introduction); ?></p>
   </div>
 </div>
+<?php endif; ?>
--- views/components/gallery.php
+++ views/components/gallery.php
@@ -3,16 +3,24 @@
     <div class="splide__track">
       <ul class="splide__list">
         <?php if (!empty($clinic_data->image1_link)) : ?>
-        <li class="splide__slide" style="background-image: url('<?php echo esc_url($clinic_data->image1_link); ?>'); background-size: cover; min-height: 400px;"></li>
+        <li class="splide__slide" style="min-height: 400px;">
+          <img src="<?php echo esc_url($clinic_data->image1_link); ?>" alt="[<?php echo esc_attr($clinic_data->clinic_name); ?>]紹介画像1" style="width: 100%; height: 100%; object-fit: cover;">
+        </li>
         <?php endif; ?>
         <?php if (!empty($clinic_data->image2_link)) : ?>
-        <li class="splide__slide" style="background-image: url('<?php echo esc_url($clinic_data->image2_link); ?>'); background-size: cover; min-height: 400px;"></li>
+        <li class="splide__slide" style="min-height: 400px;">
+          <img src="<?php echo esc_url($clinic_data->image2_link); ?>" alt="[<?php echo esc_attr($clinic_data->clinic_name); ?>]紹介画像2" style="width: 100%; height: 100%; object-fit: cover;">
+        </li>
         <?php endif; ?>
         <?php if (!empty($clinic_data->image3_link)) : ?>
-        <li class="splide__slide" style="background-image: url('<?php echo esc_url($clinic_data->image3_link); ?>'); background-size: cover; min-height: 400px;"></li>
+        <li class="splide__slide" style="min-height: 400px;">
+          <img src="<?php echo esc_url($clinic_data->image3_link); ?>" alt="[<?php echo esc_attr($clinic_data->clinic_name); ?>]紹介画像3" style="width: 100%; height: 100%; object-fit: cover;">
+        </li>
         <?php endif; ?>
         <?php if (!empty($clinic_data->image4_link)) : ?>
-        <li class="splide__slide" style="background-image: url('<?php echo esc_url($clinic_data->image4_link); ?>'); background-size: cover; min-height: 400px;"></li>
+        <li class="splide__slide" style="min-height: 400px;">
+          <img src="<?php echo esc_url($clinic_data->image4_link); ?>" alt="[<?php echo esc_attr($clinic_data->clinic_name); ?>]紹介画像4" style="width: 100%; height: 100%; object-fit: cover;">
+        </li>
         <?php endif; ?>
       </ul>
     </div>
