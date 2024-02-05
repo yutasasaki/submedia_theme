d6a2d20ba8b70c50164dcf72f54762d71e37e8b9
yutasasaki <yuta.sasaki@fortico.jp>
2024-02-05 17:27:20 +0900

    大川さんversionのkireilign v2クリニックの実装

--- shortcode/outro.php
+++ shortcode/outro.php
@@ -1,14 +1,22 @@
-<?php
-function show_outro_area_shortcode($atts) {
-    // ショートコード属性がある場合追加
-    // $atts = shortcode_atts(array(
-    //      'area' => '', // 
-    // ), $atts);
-    
+<?php 
+function show_outro_shortcode($atts, $view) {
+    // ショートコード属性を解析
+    $atts = shortcode_atts(array(
+        
+    ), $atts);
 
     // ビューファイルの読み込み
     ob_start(); // 出力バッファリング開始
-    include(get_stylesheet_directory() . '/views/outro/outro_area.php');
+    include(get_stylesheet_directory() . '/views/outro/' . $view . '.php');
     return ob_get_clean(); // バッファの内容を取得し、バッファを消去
 }
+
+function show_outro_area_shortcode($atts) {
+    return show_outro_shortcode($atts, 'area');
+}
 add_shortcode('show_outro_area', 'show_outro_area_shortcode');
+
+function show_outro_kireilign_v2_shortcode($atts) {
+    return show_outro_shortcode($atts, 'kireilign_v2');
+}
+add_shortcode('show_outro_kireilign_v2', 'show_outro_kireilign_v2_shortcode');
---
+++ views/outro/kireilign_v2.php
@@ -0,0 +1,55 @@
+<!-- wp:sgb/headings {"headingText":"さいごに","headingStyle":"sgb-heading\u002d\u002dtype2","headingIconName":"chevron-circle-right","headingTextColor":"#FFF","headingIconColor":"rgba(255,255,255,.6)","headingBgColor1":"#009EF3"} -->
+<h2 class="wp-block-sgb-headings sgb-heading"><span class="sgb-heading__inner sgb-heading--type2 with-icon"
+    style="background-color:#009EF3;border-color:transparent;font-size:1.2em"><i class="fa fa-chevron-circle-right"
+      style="color:rgba(255,255,255,.6)"></i><span class="sgb-heading__text" style="color:#FFF">さいごに</span></span></h2>
+<!-- /wp:sgb/headings -->
+
+<!-- wp:paragraph -->
+<p><strong>「いつか矯正したいけど、</strong><br><strong>歯医者に行くのはまた後でいいや…」</strong><br>という方へ。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>もしあなたが、少しでも歯並びを治したいと思っているなら<br>ぜひ"今"、初診の予約を取ることをオススメします。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>矯正歯科治療では、費用や期間など<br>歯科医師にしか分からないことも多いです。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>たしかに初診に行くのは少し面倒だし、<br>どの矯正歯科に行けばいいか迷うかもしれません。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>でも、今少しだけ勇気を出して<br>"予約をする"という小さな一歩を踏み出せたら、</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>将来の自分が必ず<br><strong><span class="keiko_yellow">「あの</span><span class="keiko_yellow">とき</span><span
+      class="keiko_yellow">矯正を始めてよかった！」</span></strong><br>と喜んでくれるはずです。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>想像してみてください。</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>矯正が終わった日、鏡に映る自分を見て<br>「歯並びキレイになった！」<br>「矯正してよかった！」<br>と思えてたら、嬉しいと思いませんか？</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong><span class="keiko_yellow">少しでも矯正しようと思った"今"が<br>自分を変える最大のチャンスです！</span></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>ぜひ一度、初診に行ってみてください。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>\キレイライン矯正では「お試し」も可能！/</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:sgb/btn {"btnColor":"#90d581","label":"キレイライン矯正の詳細\u003cbr\u003e＆提携クリニック一覧　を見る"} -->
+<p class="wp-block-sgb-btn sng-align-left"><a style="background-color:#90d581" class="btn normal raised"
+    href="">キレイライン矯正の詳細<br>＆提携クリニック一覧　を見る</a></p>
+<!-- /wp:sgb/btn -->
