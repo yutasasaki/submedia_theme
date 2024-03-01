14ed4f3614bbdfd65d29876943e477a2791ae994
yutasasaki <yuta.sasaki@fortico.jp>
2024-03-01 14:22:23 +0900

    pr table fix

--- models/clinic_pr_data.php
+++ models/clinic_pr_data.php
@@ -1,6 +1,6 @@
 <?php
 class ClinicPRData {
-    public $uid,
+    public 
           $place_id,
           $clinic_name,
           $catchphrase,
@@ -11,16 +11,15 @@ class ClinicPRData {
           $headline3,
           $paragraph3;
     public function __construct($row) {
-      $this->uid = $row[0] ?? '' ; //uid
-      $this->place_id = $row[1] ?? ''; //PlaceID
-      $this->clinic_name = $row[2] ?? '' ; //歯科医院名
-      $this->catchphrase = $row[3] ?? '' ; //キャッチコピー（15~30字）
-      $this->headline1 = $row[4] ?? '' ; //見出し1（10〜30字）
-      $this->paragraph1 = $row[5] ?? '' ; //文章1（100〜150字）
-      $this->headline2 = $row[6] ?? '' ; //見出し2（10〜30字）
-      $this->paragraph2 = $row[7] ?? '' ; //文章2（100〜150字）
-      $this->headline3 = $row[8] ?? '' ; //見出し3（10〜30字）
-      $this->paragraph3 = $row[9] ?? '' ; //文章3（100〜150字）
+      $this->place_id = $row[0] ?? ''; //PlaceID
+      $this->clinic_name = $row[1] ?? '' ; //歯科医院名
+      $this->catchphrase = $row[2] ?? '' ; //キャッチコピー（15~30字）
+      $this->headline1 = $row[3] ?? '' ; //見出し1（10〜30字）
+      $this->paragraph1 = $row[4] ?? '' ; //文章1（100〜150字）
+      $this->headline2 = $row[5] ?? '' ; //見出し2（10〜30字）
+      $this->paragraph2 = $row[6] ?? '' ; //文章2（100〜150字）
+      $this->headline3 = $row[7] ?? '' ; //見出し3（10〜30字）
+      $this->paragraph3 = $row[8] ?? '' ; //文章3（100〜150字）
       
     }
 
