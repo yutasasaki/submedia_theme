c520d72f2c6cb67386c78c856eb7f7fe570fbf36
yutasasaki <yuta.sasaki@fortico.jp>
2024-03-27 15:24:28 +0900

    CTAクリックの場合の分岐処理の追加

--- services/custom_cta.php
+++ services/custom_cta.php
@@ -8,8 +8,8 @@ function get_domain_from_url() {
     // 必要に応じてさらに加工することができます。
     return $host;
 }
-function generate_cta_url($clinic_cta_path , $is_check_cta = false) {
-    if (empty($clinic_cta_path) && !$is_check_cta) {
+function generate_cta_url(){
+    if (empty($clinic_cta_path)) {
         return 'https://kyosei-guide.com/a/'; // $clinic_cta_path が空の場合はトップを返す。view側で別途分岐制御
     }
 
@@ -31,14 +31,14 @@ function generate_cta_url($clinic_cta_path , $is_check_cta = false) {
 }
 
 
-function generate_click_cta_url()
+function generate_click_cta_url($is_check_cta = false) 
 {
 
   // ACFからcustom_ctaフィールドの値を取得
   $custom_cta = get_field('custom_cta');
 
   // custom_ctaが設定されている場合、その値をURLとして返す
-  if (!empty($custom_cta)) {
+  if (!empty($custom_cta)  && !$is_check_cta) {
     return esc_url($custom_cta);
   }
 
