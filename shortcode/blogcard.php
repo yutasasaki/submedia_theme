c02a3769efb51a8ae1cd282a1a347cd114d7d669
yutasasaki <yuta.sasaki@fortico.jp>
2024-05-23 19:17:44 +0900

    リンクカードの実装

--- shortcode/blogcard.php
+++ shortcode/blogcard.php
@@ -1,34 +1,92 @@
 <?php
-add_shortcode("blogcard", "blogcard_shortcode");
 
-// ブログカードを作成するショートコードを作成
-function blogcard_shortcode($atts) {
-  // ショートコード挿入時に引数として渡す"URL"から投稿IDを取得
-  $url = $atts['url'];
-  $id = url_to_postid($url);
+require_once __DIR__ . '/../services/opengraph.php';
 
-  // 投稿IDから各種データを取得
-  $title = esc_html(get_the_title($id));
-  $exerpt = get_post_field('post_excerpt', $id);
-  //抜粋記入欄が空欄の場合は、本文の先頭から100文字を取得
-  if(empty($exerpt)) {
-    $exerpt = wp_trim_words(get_post_field('post_content', $id), 100);
+// ショートコードを追
+add_shortcode("blogcard", "show_blogcard");
+
+// リンクカードを表示する関数
+function show_blogcard($atts)
+{
+  // ショートコード引数の初期設定
+  extract(shortcode_atts(array(
+    'url' => "",
+    'title' => "",
+    'excerpt' => ""
+  ), $atts));
+
+  // OGP情報を取得
+  $graph = OpenGraph::fetch($url);
+
+  // OGPタグからタイトルと画像URLを取得
+  $Link_title = !empty($graph->title) ? esc_html($graph->title) : esc_html($title);
+  $src = !empty($graph->image) ? esc_url($graph->image) : '';
+
+  // OGPタグからdescriptionを取得し、指定されたexcerptがあればそれを使用
+  $Link_description = !empty($graph->description) ? wp_trim_words($graph->description, 60, '…') : esc_html($excerpt);
+  if (!empty($excerpt)) {
+    $Link_description = esc_html($excerpt);
   }
-  $img = wp_get_attachment_image_src(get_post_thumbnail_id($id));
-  $img_link = esc_html($img[0]);
-  $date = esc_html(get_the_date('Y.m.d', $id));
 
-  // ブログカードとして挿入するHTMLタグを$cardに代入
-  $card = '<div class="blogcard_shortcode">
-    <a href="'.$url.'">
-      <div style="background: url('.$img_link.') no-repeat center/cover"></div>
-      <div>
-        <p>'.$title.'</p>
-        <p>'.$exerpt.'</p>
-        <p>'.$date.'</p>
+  // 画像を背景画像として設定するHTMLを生成
+  $xLink_img_style = !empty($src) ? 'style="background-image: url(' . $src . ');"' : '';
+
+  // リンクカードのHTMLを生成
+  $sc_Linkcard = '
+  <div class="blogcard">
+    <a href="' . esc_url($url) . '" target="_blank">
+      <div class="blogcard_thumbnail" ' . $xLink_img_style . '></div>
+      <div class="blogcard_content">
+        <p class="blogcard_title">' . $Link_title . '</p>
+        <p class="blogcard_excerpt">' . $Link_description . '</p>
+        <div class="blogcard_link">' . esc_url($url) . '</div>
       </div>
     </a>
   </div>';
 
-  return $card;
+  return $sc_Linkcard;
+}
+
+// ショートコードを追
+add_shortcode("linkcard", "show_linkcard");
+
+// リンクカードを表示する関数
+function show_linkcard($atts)
+{
+  // ショートコード引数の初期設定
+  extract(shortcode_atts(array(
+    'url' => "",
+    'title' => "",
+    'excerpt' => ""
+  ), $atts));
+
+  // OGP情報を取得
+  $graph = OpenGraph::fetch($url);
+
+  // OGPタグからタイトルと画像URLを取得
+  $Link_title = !empty($graph->title) ? esc_html($graph->title) : esc_html($title);
+  $src = !empty($graph->image) ? esc_url($graph->image) : '';
+
+  // OGPタグからdescriptionを取得し、指定されたexcerptがあればそれを使用
+  $Link_description = !empty($graph->description) ? wp_trim_words($graph->description, 60, '…') : esc_html($excerpt);
+  if (!empty($excerpt)) {
+    $Link_description = esc_html($excerpt);
+  }
+
+  // 画像を背景画像として設定するHTMLを生成
+  $xLink_img_style = !empty($src) ? 'style="background-image: url(' . $src . ');"' : '';
+
+  // リンクカードのHTMLを生成
+  $sc_Linkcard = '
+  <div class="linkcard_wrapper">
+  <div class="linkcard">
+    <p class="linkcard_label">あわせて読みたい</p>
+    <a href="' . esc_url($url) . '" target="_blank">
+      <div class="linkcard_thumbnail" ' . $xLink_img_style . '></div>
+      <p class="linkcard_title">' . $Link_title . '</p>
+    </a>
+  </div>
+  </div>';
+
+  return $sc_Linkcard;
 }
