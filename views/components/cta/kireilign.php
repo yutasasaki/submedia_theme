4d690363ab982d8cbef07e0ac93d5f4e8e284dea
yutasasaki <yuta.sasaki@fortico.jp>
2024-05-24 13:43:19 +0900

    text color fix

--- views/components/cta/kireilign.php
+++ views/components/cta/kireilign.php
@@ -1,6 +1,6 @@
 <section class="cta bg-cta" style="background-color: <?php echo get_theme_mod('submedia_light_color_setting', '#009EF3'); ?>; border-radius: 10px; margin-bottom: 40px;">
-  <p style="letter-spacing: 0.05em;color: <?php echo get_theme_mod('submedia_light_color_setting', '#009EF3'); ?>; " class="has-text-align-center text-md font-bold mb-15"><span class="marker-yellow">＼通いやすい場所にきっとあるはず／</span></p>
-  <p style="color: <?php echo get_theme_mod('submedia_light_color_setting', '#009EF3'); ?>;" class="has-text-align-center text-sm font-bold mb-15">提携クリニック一覧は<br>公式サイトで確認できます！</p>
+  <p style="letter-spacing: 0.05em;color: <?php echo get_theme_mod('submedia_light_text_color_setting', '#009EF3'); ?>; " class="has-text-align-center text-md font-bold mb-15"><span class="marker-yellow">＼通いやすい場所にきっとあるはず／</span></p>
+  <p style="color: <?php echo get_theme_mod('submedia_light_text_color_setting', '#009EF3'); ?>;" class="has-text-align-center text-sm font-bold mb-15">提携クリニック一覧は<br>公式サイトで確認できます！</p>
   <!-- wp:html -->
   <figure class="wp-block-image mb-10" style="border-radius: 10px; border:4px solid; border-color: white;">
     <a data-position="intro_banner" class="" data-cta="kireilign" href="<?php echo esc_url(generate_click_cta_url()); ?>">
