9654ccdcbe14de9c5d6309ca2a62a3d7c3a791a3
yutasasaki <yuta.sasaki@fortico.jp>
2024-08-26 14:48:26 +0900

    APiキャッシュを作成

--- shortcode/clinic_info_api.php
+++ shortcode/clinic_info_api.php
@@ -1,40 +1,80 @@
 <?php
-function fetch_api_clinic($request) {
-    // APIのURLを設定
-    $api_url = 'https://kyosei-guide.co.jp/a/api/clinic/index/'.$request["slug"] ?? ''; // APIのURLに適宜置き換えてください
-    // cURLを使用してAPIからデータを取得
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $api_url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    if ($response) {
-      // $responseを<div id="dc_response">タグで囲む
-      return '<section id="dc_response"><div id="dc_response_'.$request["slug"].'" >' . $response . '</div></section>';
-    } else {
-      return '<section id="dc_response"><div id="dc_response'.$request["slug"].'" >APIからデータを取得できませんでした。</div></section>'; // エラーメッセージを返す
-    }
+function fetch_api_clinic($request)
+{
+  // キャッシュキーを生成
+  $cacheKey = 'clinic_' . md5($request["slug"]);
+  $cacheExpiration = 7 * 24 * 60 * 60; // 7日間
+
+  // キャッシュが存在する場合は、それを返す
+  $cachedResponse = get_transient($cacheKey);
+  if ($cachedResponse) {
+    return '<section id="dc_response"><div id="dc_response_' . $request["slug"] . '" >' . $cachedResponse . '</div></section>';
+  }
+
+
+  // 初期のローディングメッセージを返す
+  $output = '<section id="dc_response"><div id="dc_response_' . $request["slug"] . '">読み込み中...</div></section>';
+
+  // APIのURLを設定
+  $api_url = 'https://kyosei-guide.co.jp/a/api/clinic/index/' . $request["slug"] ?? '';
+
+  // cURLを使用してAPIからデータを取得
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+
+  if ($response) {
+    // 成功した場合、レスポンスをキャッシュに保存
+    set_transient($cacheKey, $response, $cacheExpiration);
+    $output .= '<div id="dc_response_' . $request["slug"] . '" >' . $response . '</div>';
+  } else {
+    $output .= '<div id="dc_response_' . $request["slug"] . '" >APIからデータを取得できませんでした。</div>';
+  }
+
+  return $output;
 }
 add_shortcode('api_clinic', 'fetch_api_clinic');
 
-function fetch_api_prefecture($request) {
-    // parameter check
-    $is_reservable = $request['is_reservable'] ?? false;
-    // APIのURLを設定
-    $api_url = 'https://kyosei-guide.co.jp/a/api/prefecture/'.$request["slug"] ?? '' . '?is_reservable='.$is_reservable; // APIのURLに適宜置き換えてください
-    // cURLを使用してAPIからデータを取得
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $api_url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    if ($response) {
-      // $responseを<div id="dc_response" >タグで囲む
-      return '<section id="dc_response"><div id="dc_response'.$request["slug"].'" >' . $response . '</div></section>';
-    } else {
-      return '<section id="dc_response"><div id="dc_response'.$request["slug"].'" >APIからデータを取得できませんでした。</div></section>'; // エラーメッセージを返す
-    }
+function fetch_api_prefecture($request)
+{
+  // キャッシュキーを生成
+  $cacheKey = 'prefecture_' . md5($request["slug"] . $request['is_reservable']);
+  $cacheExpiration = 7 * 24 * 60 * 60; // 7日間
+
+  // キャッシュが存在する場合は、それを返す
+  $cachedResponse = get_transient($cacheKey);
+  if ($cachedResponse) {
+    return '<section id="dc_response"><div id="dc_response_' . $request["slug"] . '" >' . $cachedResponse . '</div></section>';
+  }
+
+  $is_reservable = $request['is_reservable'] ?? false;
+
+  // 初期のローディングメッセージを返す
+  $output = '<section id="dc_response"><div id="dc_response_' . $request["slug"] . '">読み込み中...</div></section>';
+
+  // APIのURLを設定
+  $api_url = 'https://kyosei-guide.co.jp/a/api/prefecture/' . $request["slug"] ?? '' . '?is_reservable=' . $is_reservable;
+
+  // cURLを使用してAPIからデータを取得
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  if ($response) {
+    // 成功した場合、レスポンスをキャッシュに保存
+    set_transient($cacheKey, $response, $cacheExpiration);
+    $output .= '<div id="dc_response_' . $request["slug"] . '" >' . $response . '</div>';
+  } else {
+    $output .= '<div id="dc_response_' . $request["slug"] . '" >APIからデータを取得できませんでした。</div>';
+  }
+
+  return $output;
 }
 add_shortcode('api_prefecture', 'fetch_api_prefecture');
--- views/components/default/cta_kireilign.php
+++ views/components/default/cta_kireilign.php
@@ -1,5 +1,5 @@
 <!-- wp:html -->
 <div class="text-center">
-  <a data-position="casette" data-cta="kireilign" class="flex-row items-center text-sm mx-auto inline-flex sm-cta-btn click_cta" href="<?php echo esc_url(generate_click_cta_url(true)); ?>">キレイライン矯正について詳しく見てみる</a>
+  <a data-position="casette" data-cta="kireilign" data-url="<?php echo esc_url(generate_click_cta_url(true)); ?>" class="flex-row items-center text-sm mx-auto inline-flex sm-cta-btn click_cta" href="<?php echo esc_url(generate_click_cta_url(true)); ?>">キレイライン矯正について詳しく見てみる</a>
 </div>
 <!-- /wp:html -->
